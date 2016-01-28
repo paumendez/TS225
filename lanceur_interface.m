cf69011c0e3c443a672c24f94777545228a147df
paumendez <paul.mendez33850@laposte.net>
2016-01-28 15:29:17 +0100

    lanceur de l'interface graphique

---
+++ lanceur_interface.m
@@ -0,0 +1,9 @@
+%LOSTANLEN Mateo/MENDEZ Paul
+%% TS225 traitement de l image
+%%HELP::
+%Ferme les fenetres nettoie la fenetre de commande et lance l'interface
+clear all
+close all
+clc;
+
+interface;
